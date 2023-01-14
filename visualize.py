ba1f12d8438da1df293af5b1b7d59dcbed3f6598
mikasso <mikolaj.szaroleta99@gmail.com>
2023-01-14 16:12:41 +0100

    simple visualization of statistics

--- visualize.py
+++ visualize.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("clustering_2/per_label/0.csv")
+df = pd.read_csv("clustering_2/per_label/2.csv")
 df = df.set_index("k").drop(columns=df.columns[0], axis=1)
 print(df)
-df[["inertia", "adj_inertia"]].plot()
-df[["silhouette_score", "adj_silhouette_score"]].plot()
+df[["inertia"]].diff(1).plot(title="inertia diff")
+df[["silhouette_score"]].plot()
+df[["biggest_cluster_size"]].plot()
 plt.show()
